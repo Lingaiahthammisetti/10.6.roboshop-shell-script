b33e0842bf19d3196a41ead7fe1e1567e5e052a7
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-08 16:43:41 +0530

    roboshop shell script

--- user.sh
+++ user.sh
@@ -75,7 +75,7 @@ VALIDATE $? "Enable user"
 systemctl start user &>> $LOGFILE
 VALIDATE $? "Start user"
 
-cp /home/ec2-user/3.7.roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp /home/ec2-user/10.6.roboshop-shell-script/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? "Copying mongo repo"
 
 dnf install -y mongodb-mongosh &>> $LOGFILE
